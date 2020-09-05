9b6862c28e144938fff369e0c0b57440ff7ae409
Kinzamalik <kiinza.malik@gmail.com>
2020-09-06 00:20:40 +0500

    first commit

---
+++ test.js
@@ -0,0 +1,35 @@
+const age = 55;
+// const doesDrive = true;
+// if(doesDrive && age > 18){
+//     console.log("You can Drive");
+// }else{
+//     console.log("You cannot Drive");
+// } 
+
+// console.log(age == 45? 'Age is 45':'Age is not 45');
+switch(age){
+    case 19:
+        console.log("You are 19");
+        break;
+
+    case 29:
+        console.log("You are 29");
+        break;
+
+    case 39:
+        console.log("You are 39");
+        break;
+    default:
+        console.log("You are 19");
+        break;
+}
+
+
+
+
+
+
+
+
+
+ 
